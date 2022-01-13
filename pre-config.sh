df0375646b18e979180ef83c851a3c53ebedb46d
zregle <zregle@163.com>
2022-01-13 20:14:39 +0800

    configure options change: 1.enable bdevperf(remove --disable-test) 2.enable fio_plugin

--- pre-config.sh
+++ pre-config.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-./configure --disable-tests \
-        --disable-unit-tests \
+./configure --disable-unit-tests \
         --without-crypto \
         --without-vhost \
         --without-pmdk \
         --without-rbd \
         --without-vtune \
+        --with-fio=/home/lzr/fio \
         --enable-debug
